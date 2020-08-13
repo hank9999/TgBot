2d478f8bcb09afe659eeaccc01e2f55bd1772275
hank9999 <hank9999@qq.com>
2020-08-13 20:04:45 +0800

    fix config post empty

--- Setting.py
+++ Setting.py
@@ -1,4 +1,4 @@
-WebServerSetting = {'post': '', 'host': '127.0.0.1', 'port': 8080}
+WebServerSetting = {'post': 'post', 'host': '127.0.0.1', 'port': 8080}
 RconSetting = {
     'XXX1': {'address': '', 'password': '', 'port': 25575},
     'XXX2': {'address': '', 'password': '', 'port': 25576}
