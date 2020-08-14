a1eaf606d7c425233e188532201924dfa81fa141
hank9999 <hank9999@qq.com>
2020-08-14 11:24:41 +0800

    say done

--- libs/CommandExecute.py
+++ libs/CommandExecute.py
@@ -65,6 +65,53 @@ async def runCommand(message, message_id):
     return
 
 
+async def sayCommand(message, message_id):
+    if message.find('/say help') == 0:
+        text = (
+            '/say 帮助:\n'
+            '/say help 获取帮助\n'
+            '/say list 查看可用子服\n'
+            '/say <服务器名称> <名称> <内容>'
+        )
+        await ReplyMessage(text, message_id)
+        return
+    if message.find('/say list') == 0:
+        text = ''
+        for i in RconSetting.keys():
+            text += str(i) + '\n'
+        await ReplyMessage(text, message_id)
+        return
+    try:
+        args = message[5:]
+        servername = args[:args.find(' ')]
+        name_texts = args[args.find(' ')+1:]
+        name = name_texts[:args.find(' ')]
+        texts = name_texts[args.find(' ')+1:]
+    except Exception:
+        text = '参数不全, 请使用 /say help 查看帮助'
+        await ReplyMessage(text, message_id)
+        return
+
+    if args == '' or servername == '' or name_texts == '' or name == '' or texts == '' or name_texts.find(' ') < 0:
+        text = '参数不全, 请使用 /run help 查看帮助'
+        await ReplyMessage(text, message_id)
+        return
+
+    rcon_info = {}
+    for k, v in RconSetting.items():
+        if servername.lower() == str(k).lower():
+            rcon_info = v
+    if rcon_info == {}:
+        text = '不存在此子服 请执行 /say list 查看可用子服'
+        await ReplyMessage(text, message_id)
+        return
+
+    command = 'tellraw @a {"extra":[{"text":"§a"},{"clickEvent":{"action":"suggest_command","value":"[TG]' + name + '"},"text":"§f[TG] ' + name + '§r: "},{"text":"§f"},{"text":"§f' + texts + '"}],"text":""}'
+    await rcon(rcon_info['address'], rcon_info['port'], rcon_info['password'], command)
+    await ReplyMessage('已发送', message_id)
+    return
+
+
 async def parser(data):
     print(data)
 
@@ -90,3 +137,5 @@ async def parser(data):
                 await listCommand(message_id)
             elif text.find('/run') == 0:
                 await runCommand(text, message_id)
+            elif text.find('/say') == 0:
+                await sayCommand(text, message_id)
